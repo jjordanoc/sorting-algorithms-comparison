06a358f1386c9a3ddefb278121037b05682aee77
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2022-08-16 17:46:04 -0500

    <feat> finished comparison plot

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "counting_sort.h"
+#include "intro_sort.h"
 #include "selection_sort.h"
 
 template<int size>
@@ -13,14 +14,13 @@ void print_array(int (&a)[size]) {
 
 int main() {
     // ejemplo 1
-    int a1[] = {18, 17, 6, 2, 18, 6, 14};
+    int a1[] = {18, 17, 6, 2, 18, 6, 14, 0, 1};
     print_array(a1);
-    selection_sort(a1, 7);
+    counting_sort(a1, 9);
     print_array(a1);
     // ejemplo 2
     int a2[] = {39, 34, 11, 17, 42, 40, 44, 31, 19};
     print_array(a2);
-    selection_sort(a2, 9);
+    counting_sort(a2, 9);
     print_array(a2);
 }
-
